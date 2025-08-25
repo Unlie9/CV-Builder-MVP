204a210ebe8d6be9f1256287d3780b604074f05a
unlie9 <unlie.devops9@gmail.com>
2025-08-25 15:38:11 +0300

    add crud api for cv

--- app/backend/apps/cv/serializers.py
+++ app/backend/apps/cv/serializers.py
@@ -1,4 +1,5 @@
 
+from django.db import transaction
 from rest_framework import serializers
 
 from apps.cv.models import (
@@ -8,22 +9,73 @@ from apps.cv.models import (
 )
 
 
-class CvListSerializer(serializers.ModelSerializer):
-
+class HeaderSerializer(serializers.ModelSerializer):
   class Meta:
-    model = CV
+    model = Header
     fields = (
       'id',
-      'filename'
+      'full_name',
+      'position',
+      'linkedin_url'
     )
 
 
-class CvCreateSerializer(serializers.ModelSerializer):
+class BodySectionsSerializer(serializers.ModelSerializer):
+  id = serializers.IntegerField(required=False)
+
+  class Meta:
+    model = BodySection
+    fields = (
+      'id',
+      'name',
+      'description'
+    )
+
+
+class CvSerializer(serializers.ModelSerializer):
+  header = HeaderSerializer()
+  body_sections = BodySectionsSerializer(many=True, required=False)
 
   class Meta:
     model = CV
     fields = (
       'id',
       'filename',
-      'header'
+      'header',
+      'body_sections'
     )
+
+  @transaction.atomic()
+  def create(self, validated_data):
+    header_data = validated_data.pop('header')
+    body_sections_data = validated_data.pop('body_sections', [])
+
+    header = Header.objects.create(**header_data)
+    cv = CV.objects.create(header=header, **validated_data)
+
+    for body_section_data in body_sections_data:
+      BodySection.objects.create(cv_id=cv.id, **body_section_data)
+
+      return cv
+
+  @transaction.atomic()
+  def update(self, instance, validated_data):  
+    header_data = validated_data.pop('header')
+    body_sections_data = validated_data.pop('body_sections', [])
+   
+    if header_data:
+      instance.header.update(**header_data)
+
+    if body_sections_data:
+      for body_section_data in body_sections_data:
+          
+          section_id = body_section_data.get('id', None)
+
+          if section_id:
+              BodySection.objects.filter(id=section_id, cv_id=instance.id).update(**body_section_data)
+          else:
+              BodySection.objects.create(cv_id=instance.id, **body_section_data)
+
+    instance.update(**validated_data)
+      
+    return instance
--- app/backend/apps/cv/views.py
+++ app/backend/apps/cv/views.py
@@ -3,33 +3,22 @@ from rest_framework import status, viewsets
 from django.views import generic
 
 from apps.cv.models import CV
-from apps.cv.serializers import (
-  CvListSerializer,
-  CvCreateSerializer
-)
+from apps.cv.serializers import CvSerializer
 
 
 class IndexView(generic.ListView):
   queryset = CV.objects.select_related('header').prefetch_related('body_sections').all()
   template_name = 'index/index.html'      
 
+
 class CvDetailView(generic.DetailView):
   model = CV
   template_name = 'cv_detail/detail.html'
 
 
-
 class CvViewSet(viewsets.ModelViewSet):
   model = CV
   queryset = CV.objects.select_related('header').prefetch_related('body_sections').all()
-  serializer_action_classes = {
-    'list': CvListSerializer,
-    'create': CvCreateSerializer,
-    'retrieve': '',
-    'update': ''
-  }
-
-  def get_serializer_class(self):
-    return self.serializer_action_classes[self.action]
+  serializer_class = CvSerializer
 
 
